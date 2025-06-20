3a52ca818607cff71c5e18b02a81e98245d78563
prakhar-srivastava10 <thomasrogers13681@gmail.com>
2025-06-21 02:09:20 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("v1").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/about.html",
+        "/addHabitType.html",
+        "/congrats.html",
+        "/createHabit.html",
+        "/dashboard.html",
+        "/editHabit.html",
+        "/settings.html",
+        "/signup.html",
+        "/stat.html",
+        "/login.html",
+        "/css/styles.css",
+        "/js/script.js",
+        "/js/createHabit.js",
+        "/js/dashboard.js",
+        "/js/login.js",
+        "/js/editHabit.js",
+        "/js/settings.js",
+        "/js/signup.js",
+        "/js/theme.js",
+        "/images/192.jpg",
+        "/images/512.jpg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
