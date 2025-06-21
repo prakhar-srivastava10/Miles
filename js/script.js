df2a7fb8052a0c91a105f43df37647198a413776
prakhar-srivastava10 <thomasrogers13681@gmail.com>
2025-06-21 14:00:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Total duration: 3 seconds delay + 1.5 seconds fade out = 4.5 seconds
   setTimeout(function () {
     // Redirect to the login page after the splash screen animation completes
-    window.location.href = 'login.html';
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+const targetPage = isLoggedIn ? "dashboard.html" : "login.html";
+window.location.href = targetPage;
+
   }, 4800);
 });
 if ("serviceWorker" in navigator) {
